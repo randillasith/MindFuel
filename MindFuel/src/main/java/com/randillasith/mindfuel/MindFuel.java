2a1643b9cf76c4f0fe7f761dd4c8323cac5a06ed
randillasith <randillasith@icloud.com>
2024-06-08 17:58:05 +0530

    UI

---
+++ MindFuel/src/main/java/com/randillasith/mindfuel/MindFuel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.randillasith.mindfuel;
+
+
+
+/**
+ *
+ * @author Randil
+ */
+public class MindFuel {
+
+    public static void main(String[] args) {
+       // new Login().setVisible(true);
+    }
+}
